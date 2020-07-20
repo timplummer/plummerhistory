f3713e67601922f3714ca2ca107faa7526fe91ea
timplummer <tplummer84@gmail.com>
2020-07-20 01:30:41 +0000

    Deploying to gh-pages from  @ 1a63ad8d8acd3b72ec789f485e04356e07856d70 🚀

---
+++ master/precache-manifest.6619a8c06c5938acffb7198f6de78cf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b71c5e4c27a6020e41fc15e8d2ee81e",
+    "url": "/plummerhistory/master/index.html"
+  },
+  {
+    "revision": "449b9c161859c8f1e286",
+    "url": "/plummerhistory/master/static/css/main.d18dee0f.chunk.css"
+  },
+  {
+    "revision": "daee5ddbb3dc027a5c22",
+    "url": "/plummerhistory/master/static/js/2.04b6faf3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/plummerhistory/master/static/js/2.04b6faf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449b9c161859c8f1e286",
+    "url": "/plummerhistory/master/static/js/main.5d41c8cb.chunk.js"
+  },
+  {
+    "revision": "67230fa0932a642466dd",
+    "url": "/plummerhistory/master/static/js/runtime-main.03f251f5.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/plummerhistory/master/static/media/logo.8dad8028.svg"
+  }
+]);
